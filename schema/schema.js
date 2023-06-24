bff16d4001066a1f6c7146e68141627fcf3fb790
Aayush Agarwal <crazy_nuclei@Aayushs-MacBook-Air.local>
2023-06-24 21:43:41 +0530

    Added books field in AuthorType

--- schema/schema.js
+++ schema/schema.js
@@ -3,18 +3,23 @@ const {GraphQLObjectType,
      GraphQLString, 
      GraphQLSchema,
      GraphQLID, 
-     GraphQLInt} = graphql;
+     GraphQLInt, 
+     GraphQLList} = graphql;
 
-const books = [
-    {name : 'AA', genre : 'F', id : '1', authorId : "1" },
-    {name : 'BB', genre : 'S', id : '2', authorId : "2" },
-    {name : 'CC', genre : 'S', id : '3', authorId : "3" }
+// dummy data
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 
-const authors = [
-    {name : "A", age : 1, id : "1"},
-    {name : "B", age : 2, id : "2"},
-    {name : "C", age : 3, id : "3"}
+var authors = [
+    { name: 'Patrick Rothfuss', age: 44, id: '1' },
+    { name: 'Brandon Sanderson', age: 42, id: '2' },
+    { name: 'Terry Pratchett', age: 66, id: '3' }
 ];
 
 const BookType = new GraphQLObjectType({
@@ -42,7 +47,19 @@ const AuthorType = new GraphQLObjectType({
     fields : () => ({
         id : {type : GraphQLID},
         name : {type : GraphQLString},
-        age : {type : GraphQLInt}
+        age : {type : GraphQLInt},
+        books : {
+            type : new GraphQLList(BookType),
+            resolve(parent, args) {
+               var b1 = [];
+               books.forEach(bo => {
+                if(bo.authorId == parent.id) {
+                    b1.push(bo);
+                }
+               });
+               return b1;
+            }
+        }
     })
 });
 
