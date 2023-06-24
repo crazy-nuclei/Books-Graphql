04e48556c2a81f8a06234b4a73f037b54d1574c7
Aayush Agarwal <crazy_nuclei@Aayushs-MacBook-Air.local>
2023-06-24 13:48:38 +0530

    Initial graphql schema

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
+
+app.use("/graphql", graphqlHttp({
+    schema 
+}));
 
 module.exports = app;
 
